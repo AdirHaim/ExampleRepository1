28bcdbd9539891cbabbd5eb7747f4839648a0371
adir haim <adir713@gmail.com>
2020-06-23 01:13:33 +0300

    canged driver to remote webdriver

--- src/test/java/MyClass1.java
+++ src/test/java/MyClass1.java
@@ -1,36 +1,28 @@
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class MyClass1
 {
-	WebDriver driver;
-	
+ 
     @Test
-    public void test01() 
+    public void test01() throws MalformedURLException, InterruptedException 
     {
+    	DesiredCapabilities chromeDC = DesiredCapabilities.chrome();
+    	WebDriver driver = new RemoteWebDriver(new URL("http://192.168.99.100:4444/wd/hub"), chromeDC);
+    			
   	    System.setProperty("webdriver.chrome.driver", "C:/SeleniumDrivers/chromedriver.exe");
-        driver = new ChromeDriver();
         driver.manage().window().maximize(); 
         driver.get("https://www.google.co.il");  
-    }
-
-    @BeforeClass
-    public void beforeClass() 
-    {
-
-        
-    }
-
-    @AfterClass
-    public void afterClass() throws InterruptedException 
-    {
-    	Thread.sleep(3000);
+        Thread.sleep(3000);
         driver.quit();
         System.out.println("test01 from class 1 completed successfully");
     }
-
-  
 }
